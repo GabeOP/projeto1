7812124eee00b112dbabec2a91f94528fbf2d58b
Carlos <103267914+Carlos-H1307@users.noreply.github.com>
2023-02-03 11:43:09 -0300

    Commit Script 1
    verificaTexto()

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var paises = [
-    {name: "Brasil"},
-    {name: "Estados_Unidos"},
-    {name: "Mexico"},
-    {name: "Canada"}
+    {name: "brasil"},
+    {name: "estados unidos"},
+    {name: "mexico"},
+    {name: "canada"}
 ]
 
+var indice_resposta = 0;
+
 var imagem = document.getElementById("img");
+var input = document.getElementById("input");
+input.addEventListener("keyup", verificaTexto);
 
 function mostrarBandeira(indice){
     imagem.src = "./Imagens/" + paises[indice].name + ".png";
 }
 
-mostrarBandeira(0)
+function verificaTexto(){
+    var str_resposta = input.value;
+    if(str_resposta == paises[indice_resposta].name){
+        console.log("Acertou!");
+        mostrarBandeira(++indice_resposta);
+        input.value = "";
+    }
+}
+
+mostrarBandeira(indice_resposta)
